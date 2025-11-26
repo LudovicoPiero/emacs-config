4345218d2454cf0adf2e4bd60c9b118859fb65cb
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-26 14:59:54 +0900

    starting from 0
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pre-early-init.el
+++
@@ -1,9 +0,0 @@
-;;; pre-early-init.el --- Pre Early Init -*- no-byte-compile: t; lexical-binding: t; -*-
-
-;; Reducing clutter in ~/.emacs.d by redirecting files to ~/.emacs.d/var/
-;; IMPORTANT: This part should be in the pre-early-init.el file
-(setq minimal-emacs-var-dir (expand-file-name "var/" minimal-emacs-user-directory))
-(setq package-user-dir (expand-file-name "elpa" minimal-emacs-var-dir))
-(setq user-emacs-directory minimal-emacs-var-dir)
-
-(setq minimal-emacs-gc-cons-threshold (* 100 1024 1024)) ; 100MB during runtime

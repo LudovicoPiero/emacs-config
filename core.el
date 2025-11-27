8e4e8957371494c4324219a02e69c5d2caaf1bc8
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-27 19:39:48 +0900

    Hightlight TODO,FIXME, etc
    Source: https://www.jamescherti.com/emacs-highlight-keywords-like-todo-fixme-note/
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- core.el
+++ core.el
@@ -16,6 +16,25 @@
 ;; Configure Emacs to ask for confirmation before exiting
 (setq confirm-kill-emacs 'y-or-n-p)
 
+(defvar highlight-codetags-keywords
+  '(("\\<\\(TODO\\|FIXME\\|BUG\\|XXX\\)\\>" 1 font-lock-warning-face prepend)
+    ("\\<\\(NOTE\\|HACK\\)\\>" 1 font-lock-doc-face prepend)))
+
+(define-minor-mode highlight-codetags-local-mode
+  "Highlight codetags like TODO, FIXME..."
+  :global nil
+  (if highlight-codetags-local-mode
+      (font-lock-add-keywords nil highlight-codetags-keywords)
+    (font-lock-remove-keywords nil highlight-codetags-keywords))
+
+  ;; Fontify the current buffer
+  (when (bound-and-true-p font-lock-mode)
+    (if (fboundp 'font-lock-flush)
+        (font-lock-flush)
+      (with-no-warnings (font-lock-fontify-buffer)))))
+
+(add-hook 'prog-mode-hook #'highlight-codetags-local-mode)
+
 ;; Native compilation (compile-angel)
 (use-package compile-angel
   :demand t

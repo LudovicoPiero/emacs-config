54977351fc902cb237304aeb1d185dff0c1bd0e7
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-27 10:14:34 +0900

    remove debug-on-error
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pre-early-init.el
+++ pre-early-init.el
@@ -10,8 +10,6 @@
 
 ;;; Code:
 
-(setq debug-on-error t)
-
 ;;; Reducing clutter in ~/.emacs.d by redirecting files to ~/.emacs.d/var/
 (setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))

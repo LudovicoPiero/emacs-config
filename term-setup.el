280b33c5613577d8bd2a2b115460d3a02a80feee
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-27 09:40:34 +0900

    fix vterm keybind not working
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- term-setup.el
+++ term-setup.el
@@ -20,33 +20,28 @@
 
 (use-package vterm-toggle
   :ensure t
-  :after vterm
   :custom
   (vterm-toggle-hide-method 'delete-window)
   (vterm-toggle-scope 'project)
 
-  :init
+  :config
   (with-eval-after-load 'general
     (my-leader-def
       "t"  '(:ignore t :which-key "toggle/terminal")
       "tt" '(vterm-toggle :which-key "Toggle Terminal")
       "tv" '(vterm :which-key "Open Vterm Buffer")
-      "tV" '(vterm-other-window :which-key "Vterm (Split)")
+      "tV" '(vterm-other-window :which-key "Vterm (Split)")))
 
-      ;; Other toggles can live here too
-      "ts" '(stripspace-local-mode :which-key "Strip Space"))
-
-    :config
-    ;; Fix for vterm-toggle display behavior
-    (add-to-list 'display-buffer-alist
-                 '((lambda (buffer-or-name _)
-                     (let ((buffer (get-buffer buffer-or-name)))
-                       (with-current-buffer buffer
-                         (or (equal major-mode 'vterm-mode)
-                             (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
-                   (display-buffer-reuse-window display-buffer-at-bottom)
-                   (reusable-frames . visible)
-                   (window-height . 0.3)))))
+  ;; Fix for vterm-toggle display behavior
+  (add-to-list 'display-buffer-alist
+               '((lambda (buffer-or-name _)
+                   (let ((buffer (get-buffer buffer-or-name)))
+                     (with-current-buffer buffer
+                       (or (equal major-mode 'vterm-mode)
+                           (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
+                 (display-buffer-reuse-window display-buffer-at-bottom)
+                 (reusable-frames . visible)
+                 (window-height . 0.3))))
 
 (provide 'term-setup)
 ;;; term-setup.el ends here

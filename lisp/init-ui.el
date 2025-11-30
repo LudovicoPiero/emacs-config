32baa246369eff1bc10b1f1ddf1a75b9481baa72
Ludovico Piero <lewdovico@gnuweeb.org>
2025-11-30 20:49:53 +0900

    Change ui to Catppuccin, set modeline config
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -4,18 +4,40 @@
 (set-face-attribute 'default nil :font "Iosevka q SemiBold" :height 140)
 
 ;; -- Theme --
-(use-package doom-themes
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t))
+(use-package catppuccin-theme
+  :init
+  ;; flavors: latte, frappe, macchiato, mocha
+  (setq catppuccin-flavor 'mocha)
+  (load-theme 'catppuccin :no-confirm))
 
 ;; -- Modeline --
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
   :config
-  (setq doom-modeline-height 25))
+  ;; Mimic Doom Emacs Defaults
+  (setq doom-modeline-height 35)      ; slightly taller for the "fat" look
+  (setq doom-modeline-bar-width 4)    ; The colored bar on the left
+  (setq doom-modeline-hud nil)        ; weird visual hud, usually off
+  (setq doom-modeline-window-width-limit fill-column)
+
+  ;; File name style: /p/t/project/src/main.c
+  (setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
+
+  ;; Icons
+  (setq doom-modeline-icon t)
+  (setq doom-modeline-major-mode-icon t)
+  (setq doom-modeline-major-mode-color-icon t)
+  (setq doom-modeline-buffer-state-icon t)
+  (setq doom-modeline-buffer-modification-icon t)
+
+  ;; Segments (declutter)
+  (setq doom-modeline-minor-modes nil) ; hide minor modes (clutter)
+  (setq doom-modeline-enable-word-count nil)
+  (setq doom-modeline-buffer-encoding nil)
+  (setq doom-modeline-indent-info nil)
+  (setq doom-modeline-checker-simple t) ; simple checks (e.g. flymake)
+  (setq doom-modeline-vcs-max-length 12)
+  (setq doom-modeline-lsp t))          ; Show LSP status
 
 ;; -- Line Numbers --
 (column-number-mode)

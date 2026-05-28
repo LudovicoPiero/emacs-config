011bc8cab9310ffc0d807a01f00b54f7af017023
LudovicoPiero <44255157+LudovicoPiero@users.noreply.github.com>
2026-05-28 01:30:35 +0000

    chore: sync init.el and early-init.el from upstream

--- init.el
+++ init.el
@@ -514,6 +514,11 @@
 ;; Move +/- indicators to the fringe for cleaner diffs
 (setq diff-font-lock-prettify t)
 
+;;; Diff
+
+;; Move +/- indicators to the fringe for cleaner diffs
+(setq diff-font-lock-prettify t)
+
 ;;; Help
 
 ;; Enhance `apropos' and related functions to perform more extensive searches

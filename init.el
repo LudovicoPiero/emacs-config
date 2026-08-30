b2b821c625a262df443ace09ceb209ea7e3873d2
LudovicoPiero <44255157+LudovicoPiero@users.noreply.github.com>
2026-08-30 01:52:48 +0000

    chore: sync init.el and early-init.el from upstream

--- init.el
+++ init.el
@@ -477,7 +477,6 @@ This should be called after changing `auto-save-list-file-prefix'."
 ;;; Dired and ls-lisp
 
 (setq dired-free-space nil
-      dired-dwim-target t  ; Propose a target for intelligent moving/copying
       dired-deletion-confirmer 'y-or-n-p
       dired-filter-verbose nil
       dired-recursive-deletes 'top
@@ -487,10 +486,6 @@ This should be called after changing `auto-save-list-file-prefix'."
       ;; Suppress Dired buffer kill prompt for deleted dirs
       dired-clean-confirm-killing-deleted-buffers nil)
 
-;; This is a higher-level predicate that wraps `dired-directory-changed-p'
-;; with additional logic. This `dired-buffer-stale-p' predicate handles remote
-;; files, wdired, unreadable dirs, and delegates to dired-directory-changed-p
-;; for modification checks.
 (setq auto-revert-remote-files nil)
 
 ;; Auto refresh Dired buffers, but only if the directory's modification time has

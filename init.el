3eaff2f0e923302cc6088216dd22813ea45e0852
LudovicoPiero <44255157+LudovicoPiero@users.noreply.github.com>
2026-09-18 01:43:07 +0000

    chore: sync init.el and early-init.el from upstream

--- init.el
+++ init.el
@@ -340,7 +340,7 @@ This should be called after changing `auto-save-list-file-prefix'."
 ;;; recentf
 
 ;; `recentf' is an that maintains a list of recently accessed files.
-(setq recentf-max-saved-items 300 ; default is 20
+(setq recentf-max-saved-items 210
       recentf-max-menu-items 15)
 
 ;;; saveplace
@@ -561,6 +561,7 @@ This should be called after changing `auto-save-list-file-prefix'."
  help-enable-completion-autoload nil
  help-enable-autoload nil
  help-enable-symbol-autoload nil
+ help-clean-buttons t
  help-window-select t)  ;; Focus new help windows when opened
 
 ;;; Eglot
